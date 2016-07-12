7a6d4984f7c987412818fd3e1aed33b6f6b56f12
Gonzalo9823 <gonzalocaballero@me.com>
2016-07-12 16:38:28 -0700

    Se conecta con firebase y crea usuarios

--- The Business Game/SignUp2ViewController.swift
+++ The Business Game/SignUp2ViewController.swift
@@ -7,33 +7,94 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
 
 class SignUp2ViewController: UIViewController {
     
     @IBAction func back(sender: AnyObject) {
         navigationController?.popViewControllerAnimated(true)
-
+        
     }
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
+    
+    @IBOutlet weak var companyName: UITextField!
+    
+    var ref = FIRDatabase.database().reference()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func gamesOption(sender: AnyObject) {
+        if companyName.text != "" {
+            FIRAuth.auth()?.createUserWithEmail(mailOfPlayer, password: passwordOfPlayer, completion: {
+                user, error in
+                if error != nil {
+                    print("ERROR!")
+                }
+                else {
+                    self.activityIndicatorView.startAnimating()
+                    var userInfo: [String: String] =
+                        [   "Name": "",
+                            "Country": "",
+                            "Mail": "",
+                            "Company Name": ""
+                    ]
+                    
+                    userInfo["Name"] = nameOfPlayer
+                    userInfo["Mail"] = mailOfPlayer
+                    userInfo["Country"] = countryOfPlayer
+                    userInfo["Company Name"] = self.companyName.text!
+                    
+                    let registerUser = self.ref.child("Users")
+                    registerUser.child("Games").child(self.companyName.text!).setValue(userInfo)
+                    self.dismissViewControllerAnimated(true, completion: {})
+                    print("WORK")
+                    
+                    
+                }
+            })
+        }
     }
-    */
-
+    
+    @IBAction func clothDesigner(sender: AnyObject) {
+        if companyName.text != "" {
+            FIRAuth.auth()?.createUserWithEmail(mailOfPlayer, password: passwordOfPlayer, completion: {
+                user, error in
+                if error != nil {
+                    print("ERROR!")
+                }
+                else {
+                    self.activityIndicatorView.startAnimating()
+                    var userInfo: [String: String] =
+                        [   "Name": "",
+                            "Country": "",
+                            "Mail": "",
+                            "Company Name": ""
+                    ]
+                    
+                    userInfo["Name"] = nameOfPlayer
+                    userInfo["Mail"] = mailOfPlayer
+                    userInfo["Country"] = countryOfPlayer
+                    userInfo["Company Name"] = self.companyName.text!
+                    
+                    let registerUser = self.ref.child("Users")
+                    registerUser.child("Cloth Designer").child(self.companyName.text!).setValue(userInfo)
+                    self.dismissViewControllerAnimated(true, completion: {})
+                    print("WORK")
+                    
+                    
+                }
+            })
+        }
+    }
 }
